c3bc1498939c7d3c7e8a7751634f0c4515214e7c
Vasco Madrid <44776047+vascomadrid@users.noreply.github.com>
2019-03-05 22:51:09 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,7 +19,17 @@ const pie = d3.pie()
 const arc = d3.arc()
     .innerRadius(0)
     .outerRadius(radius);
+/*
+var tooltip = d3.select('#chart') // select element in the DOM with id 'chart'
+  .append('div') // append a div element to the element we've selected                                    
+  .attr('class', 'tooltip'); // add class 'tooltip' on the divs we just selected
 
+tooltip.append('div') // add divs to the tooltip defined above                            
+  .attr('class', 'type'); // add class 'label' on the selection                         
+
+tooltip.append('div') // add divs to the tooltip defined above                     
+  .attr('class', 'count'); // add class 'count' on the selection                  
+*/
 
 
 function type(d) {
@@ -37,20 +47,9 @@ function arcTween(a) {
     this._current = i(1);
     return (t) => arc(i(t));
 }
-
-// define tooltip
-var tooltip = d3.select('#chart-area') // select element in the DOM with id 'chart'
-    .append('div') // append a div element to the element we've selected
-    .attr('class', 'tooltip'); // add class 'tooltip' on the divs we just selected
-
-tooltip.append('div') // add divs to the tooltip defined above
-    .attr('class', 'label'); // add class 'label' on the selection
-
-tooltip.append('div') // add divs to the tooltip defined above
-    .attr('class', 'count'); // add class 'count' on the selection
-
-tooltip.append('div') // add divs to the tooltip defined above
-    .attr('class', 'percent'); // add class 'percent' on the selection
+var div = d3.select("#ttip").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
 
 d3.json("data/data.json", type).then(data => {
     d3.selectAll("input")
@@ -70,21 +69,48 @@ d3.json("data/data.json", type).then(data => {
             .attr("d", arc)
             .attr("stroke", "white")
             .attr("stroke-width", "6px")
-            .each(function(d) { this._current = d; });
+            .each(function(d) { this._current = d; })
+	.on("mouseover", function(d) {		
+            div.transition()		
+                .duration(200)		
+                .style("opacity", .9);		
+            div	.html(function(a){
+		if(d.data.type == "speeding")
+			return "Percentage Of Drivers Involved In Fatal Collisions Who Were Speeding: " + d.data.count
+		else if(d.data.type == "alcoholImpaired")
+			return "Percentage Of Drivers Involved In Fatal Collisions Who Were Alcohol-Impaired: " + d.data.count
+		else if(d.data.type == "notDistracted")
+			return "Percentage Of Drivers Involved In Fatal Collisions Who Were Not Distracted: " + d.data.count
+		else if(d.data.type == "firstTimeCollision")
+			return "Percentage Of Drivers Involved In Fatal Collisions Who Had Not Been Involved In Any Previous Accidents: " + d.data.count
+		else
+			return "Total number of collisions per billion miles in " + d.data.type +  ": " + d.data.count;
+		})
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");	
+            })					
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0);
 
+/**
+        .on('mouseover', function(d) {  // when mouse enters div
+	      
+	 tooltip.select('.type').html(d.data.type); // set current label           
+	 tooltip.select('.count').html('$' + d.data.count); // set current count                     
+	 tooltip.style('display', 'block'); // set display                     
+	}) 
 
-        tooltip.select('.label').html(d.data.label); // set current label
-        tooltip.select('.count').html('$' + d.data.count); // set current count
-        //tooltip.select('.percent').html(percent + '%'); // set percent calculated above
-        tooltip.style('display', 'block'); // set display
+	.on('mouseout', function() { // when mouse leaves div
+    	tooltip.style('display', 'none'); // hide tooltip for that element
+	})
 
-path.on('mouseout', function() { // when mouse leaves div
-    tooltip.style('display', 'none'); // hide tooltip for that element
-});
+	.on('mousemove', function(d) { // when mouse moves	
+	    tooltip.style('top', (d3.event.layerY + 10) + 'px') // always 10px below the cursor
+        	   .style('left', (d3.event.layerX + 10) + 'px'); // always 10px to the right of the mouse
+**/
 
-path.on('mousemove', function(d) { // when mouse moves
-    tooltip.style('top', (d3.event.layerY + 10) + 'px') // always 10px below the cursor
-        .style('left', (d3.event.layerX + 10) + 'px'); // always 10px to the right of the mouse
 });
 
     }
